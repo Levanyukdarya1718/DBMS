a4ab15daa9ba278ea4bb39f4d6fbcab9220efcf6
User <User@DESKTOP-B06BQ3L>
2025-02-10 23:25:44 +0500

    IntroToDML

---
+++ DBMS/DML/SQLQuery0-SELECT.sql
@@ -0,0 +1,10 @@
+﻿USE PV_319_DLL;
+GO
+
+SELECT 
+		last_name +' ' +first_name+' '+middle_name		AS N'Ф.И.О. студента',
+		birth_date		AS N'Дата рождения',
+		[group_name]			AS N'Группа'
+
+FROM Students, Groups
+WHERE [group]=group_id;
